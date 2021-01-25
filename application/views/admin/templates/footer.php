ad78e858fe356ca8035edc70ca90f8f59c673bef
chi <cwbpf014237@gmail.com>
2021-01-25 09:28:47 +0800

    改url & adminlte3

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -1,13 +1,8 @@
-</div>
-    <!-- /.content-wrapper -->
-
-    
-  <!-- <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.1.0-pre
-    </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer> -->
+</section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+ 
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
@@ -17,5 +12,39 @@
 </div>
 <!-- ./wrapper -->
 
+<!-- jQuery -->
+<script src="/public/admin_assets/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="/public/admin_assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="/public/admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="/public/admin_assets/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="/public/admin_assets/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="/public/admin_assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/public/admin_assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="/public/admin_assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="/public/admin_assets/plugins/moment/moment.min.js"></script>
+<script src="/public/admin_assets/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="/public/admin_assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="/public/admin_assets/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="/public/admin_assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/public/admin_assets/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="/public/admin_assets/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/public/admin_assets/js/demo.js"></script>
 </body>
 </html>
--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -1,55 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?= $title ?> | BMO</title>
+  <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?=$title;?> | BMO 系統管理後台</title>
-  <base href="<?=base_url()?>">
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="./public/admin_assets/plugins/fontawesome-free/css/all.min.css">
-  <!-- pace-progress -->
-  <link rel="stylesheet" href="./public/admin_assets/plugins/pace-progress/themes/black/pace-theme-flat-top.css">
-  <!-- adminlte-->
-  <link rel="stylesheet" href="./public/admin_assets/css/adminlte.min.css">
-  <link rel="stylesheet" href="./public/admin_assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
-
-  <!-- jQuery -->
-  <script src="./public/admin_assets/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="./public/admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- pace-progress -->
-  <script src="./public/admin_assets/plugins/pace-progress/pace.min.js"></script>
-
-  <script src="./public/admin_assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="./public/admin_assets/js/adminlte.min.js"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="./public/admin_assets/js/demo.js"></script>
-
-<!-- sweetalert2 -->
-<link rel="stylesheet" href="./public/admin_assets/plugins/sweetalert2/sweetalert2.min.css">
-<script src="./public/admin_assets/plugins/sweetalert2/sweetalert2.min.js"></script>
-<!-- Vue -->
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<!-- axios -->
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-
-
+  <link rel="stylesheet" href="/public/admin_assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/public/admin_assets/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="/public/admin_assets/plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition sidebar-mini pace-primary">
-<!-- Site wrapper -->
-<div class="wrapper">
-  
 
-
-
-  <!-- Main Sidebar Container -->
-  <?php $this->load->view('admin/templates/left_menu');?>
-
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-
-
+<?php $this->load->view('admin/templates/left_menu') ?>
