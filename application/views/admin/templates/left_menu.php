0f5287038515c0e995aa150bf5f219097716b2cb
chi <cwbpf014237@gmail.com>
2021-02-24 15:18:40 +0800

    OK

--- application/views/admin/templates/left_menu.php
+++ application/views/admin/templates/left_menu.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="./admin/" class="brand-link">
         <img src="./public/admin_assets/img/BmoLogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">BMO Admins</span>
+        <span class="brand-text font-weight-light">BMO Admin</span>
     </a>
 
     <!-- Sidebar -->
